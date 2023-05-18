3c72bb71165f11b6d83ae5f0ae12434b68a0a108
minhanPark <alsgks1218@naver.com>
2023-05-18 17:57:02 +0900

    fetch test

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -1,7 +1,19 @@
-export default function Page() {
+async function getUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  console.log("res", res.ok);
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getUsers();
   return (
     <div>
-      <h1>디테일 페이지</h1>
+      {data.map((user: any) => (
+        <div key={user.id} className="mb-2">
+          <h4>{user.name}</h4>
+          <h4>{user.email}</h4>
+        </div>
+      ))}
     </div>
   );
 }
